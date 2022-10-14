f170d1a503ab9e8d0d01fb1a9832a03c0e3a79c8
CavaEditor <cabacv15@gmail.com>
2022-10-14 17:49:18 +0300

    controller

--- app/api/controllers/Controller.php
+++ app/api/controllers/Controller.php
@@ -1,7 +1,32 @@
-declare(strict_types = 1);
+<?php
 
-namespace budget\api\controllers;
+namespace budget\app\api\controllers;
+    
+
+    use Carbon\Carbon;
+    use Carbon\Exceptions\InvalidDateException;
+    use Carbon\Exceptions\InvalidFormatException;
+    use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+    use Illuminate\Foundation\Bus\DispatchesJobs;
+    use Illuminate\Foundation\Validation\ValidatesRequests;
+    use Illuminate\Routing\Controller as BaseController;
+    use League\Fractal\Manager;
+    use League\Fractal\Serializer\JsonApiSerializer;
+    use Log;
+    use Psr\Container\ContainerExceptionInterface;
+    use Psr\Container\NotFoundExceptionInterface;
+    use Symfony\Component\HttpFoundation\ParameterBag;
+
 
 abstract class Controller extends BaseController {
     
+    use Authoriz;
+
+    public function __constructor(){
+        
+    
+    }
+
+
 }
+?>
---
+++ app/api/controllers/auto/AccountController.php
@@ -0,0 +1,6 @@
+<?php
+    namespace budget\app\api\controllers\auto;
+
+    class AccountController extends Controller{}
+
+?>
